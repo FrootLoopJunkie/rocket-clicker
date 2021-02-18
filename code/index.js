7fa1488f9b47680f61e8d9889017a595d77bfef6
Kolby Evans <77802339+FrootLoopJunkie@users.noreply.github.com>
2021-02-18 14:07:53 -0600

    Sounds effects

--- code/index.js
+++ code/index.js
@@ -1,19 +1,28 @@
 const body = $("body");
-let lastMoonBucks = 0;
-let moonBucks = 0;
 const baseBuildingCostArray = [10, 75, 750, 5000, 50000, 500000, 10000000, 100000000, 1000000000]
 const baseBuildingOutputArray = [0.1, 1, 20, 100, 750, 10000, 100000, 5000000, 100000000]
 const baseBuildingCountArray = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-let   baseBuildingCostMulitplierArray = [1.0035, 1.004,  1.0045, 1.004, 1.0035,  1.003,  1.0025,  1.002,  1.0015]
+const baseBuildingCostMulitplierArray = [1.0035, 1.004,  1.0045, 1.004, 1.0035,  1.003,  1.0025,  1.002,  1.0015]
+const buyAmount = [1, 10, 100, "max"];
+const buttonClickSound = new Audio("sounds/buttonClick.wav");
+const errorSound = new Audio("sounds/error.mp3")
+const click1 = new Audio("sounds/click1.wav");
+const click2 = new Audio("sounds/click2.wav");
+const click3 = new Audio("sounds/click3.mp3");
+
+let clickSoundArray = [click1, click2, click3]
+
+let skinArray = ["images/rocket.png"]
+
+let lastMuskbucks = 0;
+let muskbucks = 0;
 let clickArray = [];
 let clickArrayIndex = 0;
-const buyAmount = [1, 10, 100, "max"];
-
 let newsArray = [];
 let newsIndex = 0;
 
-const clickSound = new Audio("sounds/click.wav");
-
+let mouseX;
+let mouseY;
 
 let buildingCostArray = baseBuildingCostArray;
 let buildingOutputArray = baseBuildingOutputArray;
@@ -25,7 +34,7 @@ let selectedBuyAmount = 0;
 let unlockedBuildings = [0, 1];
 
 
-setInterval(updateMoonbucks, 500)
+setInterval(updateMuskbucks, 500)
 
 setInterval(function(){
     checkUpgrades();
@@ -33,10 +42,15 @@ setInterval(function(){
     checkBuildingUnlocks();
 }, 1000);
 
-function buyBuilding(buildingId, howMany){
+function buyBuilding(buildingId, howMany, e){
     let baseCost = baseBuildingCostArray[parseInt(buildingId) -1]
     let totalCost = calculateFutureCost(buildingId, howMany);
-    if(moonBucks >= totalCost){
+    if(muskbucks >= totalCost){
+        buttonClickSound.play();
+        $(e.target).addClass("buttonClick")
+        $(e.target).on("animationend", function(){
+            $(e.target).removeClass("buttonClick");
+        })
         if(buildingCountArray[buildingId -1] !== 0){
             buildingCostArray[buildingId -1] = Math.ceil(baseCost * ((baseBuildingCostMulitplierArray[buildingId - 1]) ** buildingCountArray[buildingId - 1]) * howMany);
         }else{
@@ -45,11 +59,12 @@ function buyBuilding(buildingId, howMany){
         }
         
         buildingCountArray[buildingId - 1] += howMany;
-        moonBucks -= totalCost;
-        updateMoonbucks()
+        muskbucks -= totalCost;
+        updateMuskbucks()
         updateCountAndCost();
 
     }else{
+        errorSound.play();
         console.log("not enough money")
         $("#incomeStats").addClass("flashRed");
     }
@@ -57,6 +72,10 @@ function buyBuilding(buildingId, howMany){
 
 function checkUpgrades(){
     
+}
+
+function random(max, min){
+    return Math.floor(Math.random() * (max - min) + min);
 }
 
 function calculateFutureCost(buildingId, howMany){
@@ -75,7 +94,7 @@ function calculateFutureCost(buildingId, howMany){
 
 function checkBuildingUnlocks(){
     $.each(baseBuildingCostArray, function(index, value){
-        if(value <= moonBucks){
+        if(value <= muskbucks){
             let buildingToUnlockId = index + 3;
             unlockedBuildings.push(index);
             $(`#${buildingToUnlockId}`).attr("hidden", false);
@@ -89,7 +108,7 @@ function calculateOutput(){
         output += value * buildingOutputArray[index]
     })
     currentOutput = output;
-    moonBucks += output;
+    muskbucks += output;
 }
 
 function updateCountAndCost(){
@@ -103,13 +122,18 @@ function updateCountAndCost(){
     })
 }
 
-function updateMoonbucks(){
+function updateMuskbucks(){
     let newBreak = $("<br>");
-    $("#incomeStats").text(`Moonbucks: ${moonBucks.toFixed(2)}`).append(newBreak).append(`Current Output: ${currentOutput.toFixed(2)}`);
+    $("#incomeStats").text(`Muskbucks: ${muskbucks.toFixed(2)}`).append(newBreak).append(`Current Output: ${currentOutput.toFixed(2)}`);
 }
 
 $("document").ready(function(){
     updateCountAndCost();
+
+    $(document).mousemove(function(e){
+        mouseX = e.pageX;
+        mouseY = e.pageY;
+    })
 
     function animateNews(){
         $("#news").animate({
@@ -141,16 +165,36 @@ $("document").ready(function(){
 
     $("#clicker").click(function(){
         clicks++;
-        lastMoonBucks = moonBucks;
-        moonBucks++;
-        updateMoonbucks()
-        clickSound.play();
+        lastMuskbucks = muskbucks;
+        muskbucks++;
+        updateMuskbucks()
+        clickSoundArray[random(1, 3)].play();
         $("#clicker").addClass("flash");
         let moneySign = $("<div>");
-        moneySign.text("$$$").css("position", "relative")
-        moneySign.text("$$$").css("z-index", "1")
-        moneySign.text("$$$").css("pointer-events", "none")
-        $("#clickerContainer").append(moneySign)
+        moneySign.text("$");
+        moneySign.css("position", "absolute")
+        moneySign.css("z-index", "1")
+        moneySign.css("pointer-events", "none")
+        moneySign.css("color", "blue")
+        moneySign.css("left", mouseX)
+        moneySign.css("top", mouseY)
+        moneySign.css("transform", "scale(3)")
+        moneySign.addClass("shiftUp")
+        moneySign.on("animationend", function(){
+            this.remove();
+        })
+        $("body").append(moneySign)
+    })
+
+    $("#skinsSelector").click(function(){
+        let selection = skinArray[random(0, 2)]
+        $("#clicker").attr("src", selection)
+    })
+
+    $("#marsClickerRewardButton").click(function(){
+        $("#marsClickerReward").remove();
+        skinArray.push("images/mars.png");
+        $("#clicker").attr("src", "images/mars.png")
     })
 
     $("#clicker").on("animationend", function(){
@@ -161,11 +205,17 @@ $("document").ready(function(){
         $("#incomeStats").removeClass("flashRed");
     })
 
-    
+    $(".button").click(function(e){
+        buttonClickSound.play();
+        $(e.target).addClass("buttonClick")
+        $(e.target).on("animationend", function(){
+            $(e.target).removeClass("buttonClick");
+        })
+    })
 
     $(".building").click(function(e){
         if(typeof buyAmount[selectedBuyAmount] === "number"){
-            buyBuilding(e.target.id, buyAmount[selectedBuyAmount]);
+            buyBuilding(e.target.id, buyAmount[selectedBuyAmount], e);
         }else{
             console.log("Max buy not yet added")
         }     
